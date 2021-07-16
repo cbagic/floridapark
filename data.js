9eec36f7fa72b58a5307b41520bf3c9deb6eaee3
cbagic <blankess@nwfsc.edu>
2021-07-16 08:51:01 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,494 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-welcome",
+      "name": "Welcome",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.5699034292226237,
+        "pitch": -0.21196226409284336,
+        "fov": 1.290348174345596
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4805135629952044,
+          "pitch": 0.06066240038829562,
+          "rotation": 0,
+          "target": "1-boat-launch--salt-marsh"
+        },
+        {
+          "yaw": -0.7304073924094094,
+          "pitch": 0.06280911859955651,
+          "rotation": 0,
+          "target": "2-creek-to-bay"
+        },
+        {
+          "yaw": 0.4066819340363317,
+          "pitch": 0.06438448681886122,
+          "rotation": 0,
+          "target": "6-creekside-meadow"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.90194157236434,
+          "pitch": 0.11202828508246299,
+          "title": "This is a public park<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.09959995564169333,
+          "pitch": -0.07720316450495623,
+          "title": "Welcome",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.2727661783534678,
+          "pitch": -0.14094589942043356,
+          "title": "Boat Launch<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6064219157554067,
+          "pitch": -0.15312568675680538,
+          "title": "Bayou",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-boat-launch--salt-marsh",
+      "name": "Boat Launch & Salt Marsh",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.290348174345596
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9633371433625193,
+          "pitch": -0.07512189203122155,
+          "rotation": 0,
+          "target": "0-welcome"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.98662432366595,
+          "pitch": 0.035910659874064166,
+          "title": "Stump",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.4869273298759724,
+          "pitch": 0.23476610153980992,
+          "title": "Type of boats<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.706865121714646,
+          "pitch": -0.16490108225901778,
+          "title": "Private properties<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6364160959109739,
+          "pitch": 0.11496314526648099,
+          "title": "Healthy salt marsh<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.0017605948156393936,
+          "pitch": 0.34265631445887124,
+          "title": "Salt marsh extras<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-creek-to-bay",
+      "name": "Creek to Bay",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.45581897610886557,
+        "pitch": 0.31997970583730506,
+        "fov": 1.290348174345596
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5192520578076163,
+          "pitch": 0.02799082723712587,
+          "rotation": 0,
+          "target": "3-examining-reefs"
+        },
+        {
+          "yaw": -2.364152024358228,
+          "pitch": -0.05713456220145119,
+          "rotation": 0,
+          "target": "0-welcome"
+        },
+        {
+          "yaw": 2.676846340446387,
+          "pitch": -0.09916986145800166,
+          "rotation": 0,
+          "target": "6-creekside-meadow"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.858551840807099,
+          "pitch": 0.3225058288937319,
+          "title": "Rushes",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.2898960305036606,
+          "pitch": 1.0012049080772627,
+          "title": "Tannins",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.7576023562830034,
+          "pitch": 0.9598568953677198,
+          "title": "creek enters bay<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.649895801440822,
+          "pitch": -0.4266013695912303,
+          "title": "Sabal palm<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.4668335309409457,
+          "pitch": -0.11291355471446707,
+          "title": "Trees behind salt marsh<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.664651814670389,
+          "pitch": 0.005373152733634257,
+          "title": "Reefs breaking water<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.6563609671304818,
+          "pitch": 0.19565362259370644,
+          "title": "Emergent plant<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-examining-reefs",
+      "name": "Examining Reefs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.7061445876077066,
+        "pitch": -0.20673360715853661,
+        "fov": 1.290348174345596
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.878835125599883,
+          "pitch": 0.021038646855277676,
+          "rotation": 0,
+          "target": "2-creek-to-bay"
+        },
+        {
+          "yaw": 2.9525600761515234,
+          "pitch": 0.06013611634123528,
+          "rotation": 0,
+          "target": "4-reefs-around-tree"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.488562712955064,
+          "pitch": -0.1674347074903988,
+          "title": "AmeriCorps reef monitoring<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.9771106000205272,
+          "pitch": 0.285414227673634,
+          "title": "Reef materials<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.0949684597017733,
+          "pitch": 0.2217748709806635,
+          "title": "reef dimensions regulations<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.9367481047265187,
+          "pitch": 0.17993695399315435,
+          "title": "Creatures that live on reefs<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.1296624002841842,
+          "pitch": -0.12488120947246806,
+          "title": "Sailboats on water<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.1070045940186235,
+          "pitch": -0.1180552749363315,
+          "title": "Fetch",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-reefs-around-tree",
+      "name": "Reefs Around Tree",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.27604697352971286,
+        "pitch": -0.10601451883489332,
+        "fov": 1.290348174345596
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5509034367807146,
+          "pitch": -0.025648837027329563,
+          "rotation": 0,
+          "target": "5-boardwalk-view"
+        },
+        {
+          "yaw": 1.8009430131425255,
+          "pitch": -0.04123692498664333,
+          "rotation": 0,
+          "target": "3-examining-reefs"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.36704948150374506,
+          "pitch": -0.05423796558591398,
+          "title": "<div>Exposed roots</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.26336257062032153,
+          "pitch": 0.1598748214309058,
+          "title": "Erosion is bad<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.192311742893315,
+          "pitch": -0.18925836787576245,
+          "title": "Person using the dock<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.9320321248486962,
+          "pitch": 0.02714950686969786,
+          "title": "<div>Smooth cordgrass is still growing in here</div>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-boardwalk-view",
+      "name": "Boardwalk View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.290348174345596
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.5543131332908011,
+          "pitch": -0.21879268559799137,
+          "title": "Picnic tables<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.0547534389440063,
+          "pitch": -0.06925924238339576,
+          "title": "Shade tree<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.6135191296161295,
+          "pitch": 0.06307601636637017,
+          "title": "Creek",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.03284526144503097,
+          "pitch": -0.15157288001947222,
+          "title": "Fishing",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.840987334904927,
+          "pitch": -0.02991656400571685,
+          "title": "Monofilament",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-creekside-meadow",
+      "name": "Creekside Meadow",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.70882223702385,
+        "pitch": -0.1615459334626692,
+        "fov": 1.290348174345596
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.5642846517588982,
+          "pitch": -0.12031032364778227,
+          "title": "Riparian Zone<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.368532988544068,
+          "pitch": -0.4873598745043477,
+          "title": "Cypress",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.8580713694691777,
+          "pitch": 0.04897229033272765,
+          "title": "Playground",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.7901807196218869,
+          "pitch": 0.10777509613079062,
+          "title": "Fresh water<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7071519590585549,
+          "pitch": -1.019952484365021,
+          "title": "Clouds",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
