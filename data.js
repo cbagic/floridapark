ef98d5dd83380f7a468321c1a6faecfcb7a39314
cbagic <blankess@nwfsc.edu>
2021-07-23 11:03:33 -0500

    7/23 update

--- data.js
+++ data.js
@@ -48,26 +48,47 @@ var APP_DATA = {
         {
           "yaw": 2.90194157236434,
           "pitch": 0.11202828508246299,
-          "title": "This is a public park<br>",
-          "text": "Text"
+          "title": "Visit Florida Park<br>",
+          "text": "Florida Park is a public park in Valparaiso, and you can visit it to enjoy beautiful views of" +
+              " our living shoreline. Amenities include a playground, restrooms, covered picnic tables, and a boat launch. <br><br>" +
+              "<img src='https://i.imgur.com/CzBqw8c.jpg' alt='Florida Park rules' width='100%'><br><br>" +
+              "<img src='https://i.imgur.com/TYLB44D.jpg' alt='Do not disturb living shoreline' width='100%>'>",
+          "fullscreen": true
         },
         {
           "yaw": 0.09959995564169333,
           "pitch": -0.07720316450495623,
           "title": "Welcome",
-          "text": "Text"
+          "text": "This tour is brought to you by AmeriCorps NWF Environmental Stewards and <a href='http://basinalliance.org' style='color: lawngreen'> the Choctawhatchee " +
+              "Basin Alliance (CBA)</a>. Welcome to the Florida Park Living Shoreline, a restoration site established in " +
+              "cooperation with the city of Valparaiso. Here, we have built oyster reefs and planted smooth cordgrass. Explore the " +
+              "park to learn more!<br>",
+          "fullscreen": false
         },
         {
           "yaw": -1.2727661783534678,
           "pitch": -0.14094589942043356,
           "title": "Boat Launch<br>",
-          "text": "Text"
+          "text": "Florida Park includes a boat ramp. Our living shorelines improve the bay for humans as well as wildlife" +
+              " to promote swimmable, fishable waterways. If you’re using the boat ramp, be mindful of our living shoreline " +
+              "and the plants and animals present.<br>",
+          "fullscreen": false
+        },
+        {
+          "yaw": -1,
+          "pitch": -.75,
+          "title": "Get Your Bearings<br>",
+          "text": "<img src='https://i.imgur.com/QXnj6N8.png' width='100%' alt='map'>",
+          "fullscreen": true
         },
         {
           "yaw": -0.6064219157554067,
           "pitch": -0.15312568675680538,
           "title": "Bayou",
-          "text": "Text"
+          "text": "Florida Park is located on Boggy Bayou, which is connected to the Choctawhatchee Bay. Turkey Creek feeds into Boggy Bayou. On the" +
+              " map below, a gold star marks Florida Park.<br><br>" +
+              "<img src='https://i.imgur.com/FXEOtmY.png' width='100%' alt='choctawhatchee bay'>",
+          "fullscreen": true
         }
       ]
     },
@@ -108,31 +129,36 @@ var APP_DATA = {
           "yaw": -0.98662432366595,
           "pitch": 0.035910659874064166,
           "title": "Stump",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": 1.4869273298759724,
           "pitch": 0.23476610153980992,
           "title": "Type of boats<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -0.706865121714646,
           "pitch": -0.16490108225901778,
           "title": "Private properties<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -0.6364160959109739,
           "pitch": 0.11496314526648099,
           "title": "Healthy salt marsh<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -0.0017605948156393936,
           "pitch": 0.34265631445887124,
           "title": "Salt marsh extras<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         }
       ]
     },
@@ -185,43 +211,57 @@ var APP_DATA = {
           "yaw": 1.858551840807099,
           "pitch": 0.3225058288937319,
           "title": "Rushes",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
+        },
+        {
+          "yaw": 2.8,
+          "pitch": 0.3225058288937319,
+          "title": "Stream Close-Up",
+          "text": "<img src='https://i.imgur.com/50Ehn2F.jpg' width='100%' alt='stream close up'>",
+          "fullscreen": false
         },
         {
           "yaw": -2.2898960305036606,
           "pitch": 1.0012049080772627,
           "title": "Tannins",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -0.7576023562830034,
           "pitch": 0.9598568953677198,
           "title": "creek enters bay<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -2.649895801440822,
           "pitch": -0.4266013695912303,
           "title": "Sabal palm<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": 1.4668335309409457,
           "pitch": -0.11291355471446707,
           "title": "Trees behind salt marsh<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -0.664651814670389,
           "pitch": 0.005373152733634257,
           "title": "Reefs breaking water<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": 0.6563609671304818,
           "pitch": 0.19565362259370644,
           "title": "Emergent plant<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         }
       ]
     },
@@ -268,37 +308,43 @@ var APP_DATA = {
           "yaw": -2.488562712955064,
           "pitch": -0.1674347074903988,
           "title": "AmeriCorps reef monitoring<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -0.9771106000205272,
           "pitch": 0.285414227673634,
           "title": "Reef materials<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -2.0949684597017733,
           "pitch": 0.2217748709806635,
           "title": "reef dimensions regulations<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -2.9367481047265187,
           "pitch": 0.17993695399315435,
           "title": "Creatures that live on reefs<br>",
-          "text": "Text"
+          "text": "<iframe width=\"100%\" height=\"530\" src=\"https://www.youtube.com/embed/GBjo12yaQIE\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+          "fullscreen": true
         },
         {
           "yaw": 1.1296624002841842,
           "pitch": -0.12488120947246806,
           "title": "Sailboats on water<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": 2.1070045940186235,
           "pitch": -0.1180552749363315,
           "title": "Fetch",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         }
       ]
     },
@@ -345,25 +391,29 @@ var APP_DATA = {
           "yaw": 0.36704948150374506,
           "pitch": -0.05423796558591398,
           "title": "<div>Exposed roots</div>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -0.26336257062032153,
           "pitch": 0.1598748214309058,
           "title": "Erosion is bad<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -2.192311742893315,
           "pitch": -0.18925836787576245,
           "title": "Person using the dock<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": 0.9320321248486962,
           "pitch": 0.02714950686969786,
           "title": "<div>Smooth cordgrass is still growing in here</div>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         }
       ]
     },
@@ -391,37 +441,57 @@ var APP_DATA = {
         "pitch": 0,
         "fov": 1.290348174345596
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 3.5,
+          "pitch": -0.06,
+          "rotation": 0,
+          "target": "6-creekside-meadow"
+        },
+        {
+          "yaw": -1,
+          "pitch": -0.03,
+          "rotation": 0,
+          "target": "4-reefs-around-tree"
+        }
+      ],
       "infoHotspots": [
         {
           "yaw": -1.5543131332908011,
           "pitch": -0.21879268559799137,
           "title": "Picnic tables<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": 2.0547534389440063,
           "pitch": -0.06925924238339576,
-          "title": "Shade tree<br>",
-          "text": "Text"
+          "title": "Shade Tree<br>",
+          "text": "In a hot sunny climate like Florida's, it's important to have shade trees available to cool down. <br>",
+          "fullscreen": false
         },
         {
           "yaw": 0.6135191296161295,
           "pitch": 0.06307601636637017,
           "title": "Creek",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -0.03284526144503097,
           "pitch": -0.15157288001947222,
           "title": "Fishing",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -0.840987334904927,
           "pitch": -0.02991656400571685,
           "title": "Monofilament",
-          "text": "Text"
+          "text": "In water, monofilament fishing line takes 600 years to degrade. If monofilament is left in the environment," +
+              " it can trap and kill wildlife. Find monofilament recycling bin locations around the bay on " +
+              "<a href='https://mrrp.myfwc.com/bin-locations/' style='color: lawngreen'>the Monofilament Recovery and Recycling Program website</a>.",
+          "fullscreen": false
         }
       ]
     },
@@ -449,37 +519,61 @@ var APP_DATA = {
         "pitch": -0.1615459334626692,
         "fov": 1.290348174345596
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 1.9,
+          "pitch": -0.15,
+          "rotation": 0,
+          "target": "0-welcome"
+        },
+        {
+          "yaw": 3,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-creek-to-bay"
+        },
+        {
+          "yaw": 3.6,
+          "pitch": -0.1,
+          "rotation": 0,
+          "target": "5-boardwalk-view"
+        }
+      ],
       "infoHotspots": [
         {
           "yaw": 0.5642846517588982,
           "pitch": -0.12031032364778227,
           "title": "Riparian Zone<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -1.368532988544068,
           "pitch": -0.4873598745043477,
           "title": "Cypress",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": -1.8580713694691777,
           "pitch": 0.04897229033272765,
           "title": "Playground",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": 1.7901807196218869,
           "pitch": 0.10777509613079062,
           "title": "Fresh water<br>",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         },
         {
           "yaw": 0.7071519590585549,
           "pitch": -1.019952484365021,
           "title": "Clouds",
-          "text": "Text"
+          "text": "Text",
+          "fullscreen": false
         }
       ]
     }
